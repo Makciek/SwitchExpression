7898aab216fcdb09c0646a4f09d8bc5ea1eabe97
Makciek <makciek@gmail.com>
2016-09-02 23:29:26 +0200

    inital

---
+++ SwitchExpression/ExpTester/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SwitchExpression;
+
+namespace ExpTester
+{
+    class Program
+    {
+        private static ExpressionGenerator<int, string> ExpGen;
+
+        static void Main(string[] args)
+        {
+            ExpGen = new ExpressionGenerator<int, string>();
+            Console.WriteLine("Start");
+
+            ExpGen.AddCase(1, (x) => Method1(x));
+            var compiled = ExpGen.Generate();
+            compiled.Invoke(1, "1Test!");
+
+            Console.WriteLine("The End");
+            Console.Read();
+        }
+
+        public static void Method1(string methdo)
+        {
+            Console.WriteLine($"Method1: {methdo}");
+        }
+    }
+}
---
+++ SwitchExpression/SwitchExpression/ExpressionGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SwitchExpression
+{
+    public class ExpressionGenerator<TestT, T>
+    {
+        private bool _isInitialized = false;
+
+        private Expression _expression;
+        private List<SwitchCase> _cases = new List<SwitchCase>();
+        
+        public Action<TestT, T> Compiled { get; set; }
+
+
+        ParameterExpression expParameter = Expression.Parameter(typeof(TestT));
+        ParameterExpression invokeParameter = Expression.Parameter(typeof(T));
+
+        public Action<TestT, T> Generate()
+        {
+            if (_isInitialized)
+                return Compiled;
+            _expression = Expression.Switch(expParameter, _cases.ToArray());
+            Expression<Action<TestT, T>> expressionToCompile = Expression.Lambda<Action<TestT, T>>(_expression, expParameter, invokeParameter);
+            Compiled = expressionToCompile.Compile();
+            _isInitialized = true;
+            return Compiled;
+        }
+
+        public ExpressionGenerator<TestT, T> AddCase(TestT testValue, Expression<Action<T>> action)
+        {
+            var expCase = Expression.SwitchCase(
+                Expression.Invoke(action, invokeParameter),
+                Expression.Constant(testValue, typeof(TestT))
+            );
+            _cases.Add(expCase);
+            return this;
+        }
+    }
+}
